e3c43d29aa6118311fda3d1486f6bb6bffe27347
abdelilah ouslimane <abdelilah.ouslimane3740@gmail.com>
2025-09-02 15:10:40 +0200

    régle de Honner Ex 6

--- Day1/Ex5.c
+++ Day1/Ex5.c
@@ -3,7 +3,8 @@ int main(){
     int x,p;
     printf("Veuillez Saisir la valeur de X :");
     scanf("%d",&x);
-    p = 3 * (x * x * x * x *x ) + 2 * (x *x*x*x) - 5 * (x * x *x) - (x * x) + 7 * x -6;
+    // p = 3 * (x * x * x * x *x ) + 2 * (x *x*x*x) - 5 * (x * x *x) - (x * x) + 7 * x -6;
+    p = ((((3 * x +2)*x-5)*x-1)*x+7)*x-6;
     printf("la valeur du polynôme est : %d",p);
     return 0;
 
